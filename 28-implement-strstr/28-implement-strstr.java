5ee455be3343b2a456d4e16460490f05d4dfb38b
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2022-07-25 23:34:05 +0530

    Time: 1 ms (64.96%), Space: 42 MB (46.27%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -10,14 +10,6 @@ class Solution {
                     return i;
             }
         }
-        /*else if(needle.length()==haystack.length())
-        {
-            for(int i=0;i<haystack.length();i++)
-            {
-                if(haystack.substring(i,i+needle.length()).equals(needle))
-                    return i;
-            }
-        }*/
         return -1;
     }
 }

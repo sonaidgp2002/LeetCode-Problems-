32fa40d63e6b327d78e0214eeca035b1051d8460
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2022-07-26 12:31:06 +0530

    Time: 2 ms (40.28%), Space: 44 MB (64.77%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,21 +1,5 @@
 class Solution {
-    public int maxProfit(int[] prices) {/*
-        ArrayList<Integer> arr=new ArrayList<>();
-        int sum=0;
-        for(int i=0;i<prices.length;i++)
-            arr.add(prices[i]);
-        for(int i=0;i<prices.length;i++)
-        {
-            int t=arr.remove(i);
-            int j=Collections.max(arr);
-            if(t<j)
-            {
-                if(arr.get(arr.indexOf(j)+1)<j)
-                    
-            }
-            else
-                continue;
-        }*/
+    public int maxProfit(int[] prices) {
         int sum=0;
         int max=Integer.MIN_VALUE;
         for(int i=0;i<prices.length-1;i++)

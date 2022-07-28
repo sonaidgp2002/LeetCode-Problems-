2b5953084d0e2afae9b1b803bbfb9e6efbc4aefd
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2022-07-28 11:24:28 +0530

    Time: 500 ms (6.42%), Space: 46.8 MB (15.31%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,18 +11,8 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        /*
-        ListNode tail=head;
-        while(tail.next!=null)
-        {
-            
-            if()
-            tail=tail.next;
-        }*/
-        
         ListNode temp=head;
         ArrayList<ListNode> arr=new ArrayList<>();
-        ArrayList<Integer> arr1=new ArrayList<>();
         while(temp!=null)
         {
             if(arr.contains(temp))
@@ -32,15 +22,5 @@ public class Solution {
             temp=temp.next;
         }
         return false;
-        /*
-        while(temp!=null)
-        {
-            if(arr1.contains(temp.val))
-                return true;
-            else
-                arr1.add(temp.val);
-            temp=temp.next;
-        }
-        return false;*/
     }
 }

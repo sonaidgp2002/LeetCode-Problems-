bc776b71479038d44317b641eb60617867bda0c0
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2022-07-30 10:00:33 +0530

    Time: 2 ms (98.80%), Space: 42.3 MB (94.66%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,5 +1,30 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int c=0;
+        int[] arr=new int[nums1.length];
+	    HashMap<Integer, Integer> hash=new HashMap<>();
+	    for(int i=0;i<nums2.length;i++)
+	    {
+	        if(nums2[i] > nums2[c])
+	        {
+	            hash.put(nums2[c], nums2[i]);
+	            i=c;
+	            c++;
+	        }
+	        if(i+1==nums2.length && hash.size()!=nums2.length)
+	        {
+	            hash.put(nums2[c], -1);
+	            i=c;
+	            c++;
+	        }
+	    }
+        c=0;
+        for(int i=0;i<nums1.length;i++)
+        {
+            arr[c++]=hash.get(nums1[i]);
+        }
+        return arr;
+        /*
         int k=nums1.length-1;
         int c=0;
         int[] arr=new int[nums1.length];
@@ -47,7 +72,7 @@ class Solution {
                 arr[k++]=-1;
             else
                 c1=0;
-        }*/
-        return arr;
+        }
+        return arr;*/
     }
 }

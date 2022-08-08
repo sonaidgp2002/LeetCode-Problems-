1dbe3145497824f7af5da6ae5d82d322b4e06ad4
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2022-08-08 11:54:43 +0530

    Time: 3 ms (62.77%), Space: 42.3 MB (33.29%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -3,8 +3,6 @@ class Solution {
         if(s.length() == 1)
             return false;
         Stack<Character> stk = new Stack<>();
-        // Stack<Character> stk1 = new Stack<>();
-        // ArrayList<Character> arr = new ArrayList<>();
         for( int i=0; i<s.length(); i++)
         {
             if( s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[' )
@@ -17,31 +15,7 @@ class Solution {
                 if((ch != '(' && s.charAt(i) == ')' ) || (ch != '[' && s.charAt(i) == ']') || (ch != '{' && s.charAt(i) == '}'))
                     return false;
             }
-        }
-        // for(int i=s.length()-1;i>=0;i--)
-        // {
-        //     if(stk.isEmpty())
-        //         break;
-        //     if(( s.charAt(i) == ')' && stk.peek() == '(') || ( s.charAt(i) == ']' && stk.peek() == '[') || ( s.charAt(i) == '}' && stk.peek() == '{'))
-        //         stk.pop();
-        // }
-        // int i = 0;
-        // int j = arr.size() - 1;
-        // for(;i!=j && i<j; i++, j--)
-        // {
-        //     if( (arr.get(i) == '(' && arr.get(j) != ')') || (arr.get(i) == '[' && arr.get(j) != ']') || (arr.get(i) == '{' && arr.get(j) != '}') )
-        //         return false;
-        // }
-        // while(!stk.isEmpty() && !stk1.isEmpty())
-        // {
-        //     if(stk.peek() == '(' && stk.pop() + 1 != stk1.pop())
-        //         return false;
-        //     else if(stk.size() !=0 && stk1.size() != 0 && stk.pop() + 2 != stk1.pop())
-        //         return false;
-        // }
-        // if(stk.size() != 0 || stk1.size() != 0)
-        //     return false;
-        if(!stk.isEmpty())
+        }if(!stk.isEmpty())
             return false;
         return true;
     }

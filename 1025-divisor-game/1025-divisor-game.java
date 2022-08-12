a4c6d3e880842c076e7c02233ac0993f7507fb37
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2022-08-12 12:16:45 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (30.22%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,14 +1,8 @@
 class Solution {
     public boolean divisorGame(int n) {
-        int c = 0;
-        while(n != 1)
-        {
-            c++;
-            n -= 1;
-        }
-        if(c % 2 == 0)
-            return false;
-        else
+        if(n % 2 == 0)
             return true;
+        else
+            return false;
     }
 }

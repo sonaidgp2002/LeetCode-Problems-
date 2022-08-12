641f824fd675a4ac5d379ce8ee1f874514a3e20c
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2022-08-12 12:55:57 +0530

    Time: 4 ms (36.81%), Space: 48.1 MB (60.28%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -6,22 +6,6 @@ class Solution {
         {
             dp[i] = dp[i/2] + i%2;
         }
-        // int c = 2;
-        // arr[0] = 0;
-        // for(int i=1; i<=n ;i++)
-        // {
-        //     if(i % 2 != 0)
-        //         arr[i] = arr[i - 1] + 1;
-        //     else if(c == i)
-        //     {
-        //         c *= 2;
-        //         arr[i] = 1;
-        //     }
-        //     else if(i % 10 == 0 || i % 12 == 0)
-        //         arr[i] = 2;
-        //     else
-        //         arr[i] = arr[i - 1];
-        // }
         return dp;
     }
 }

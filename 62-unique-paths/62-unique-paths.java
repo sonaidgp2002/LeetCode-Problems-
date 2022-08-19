2385a8f65c64a46592fee67a908f451936f591e1
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2022-08-19 11:41:02 +0530

    Time: 1 ms (52.66%), Space: 41.3 MB (28.72%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,21 +1,35 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][] = new int[m][n];
-        for(int i=0; i<m; i++)
+        for(int i=0;i<m; i++)
+        {
             for(int j=0; j<n; j++)
-                dp[i][j] = -1;
-        return check(m-1, n-1, m, n, dp);
+            {
+                if(i==0 && j==0)
+                    dp[i][j] = 1;
+                else
+                {
+                    int down = 0, right = 0;
+                    if(i > 0)
+                        down = dp[i - 1][j];;
+                    if(j > 0)
+                        right = dp[i][j - 1];
+                    dp[i][j] = down + right;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
-    public int check(int i, int j, int m, int n, int dp[][])
-    {
-        if(i==0 || j==0)
-            return 1;
-        else if(i<0 || j<0)
-            return 0;
-        else if(dp[i][j] != -1)
-            return dp[i][j];
-        int lh = check(i-1, j, m, n, dp);
-        int rh = check(i, j-1, m, n, dp);
-        return dp[i][j] = lh + rh;
-    }
+    // public int check(int i, int j, int m, int n, int dp[][])
+    // {
+    //     if(i==0 || j==0)
+    //         return 1;
+    //     else if(i<0 || j<0)
+    //         return 0;
+    //     else if(dp[i][j] != -1)
+    //         return dp[i][j];
+    //     int lh = check(i-1, j, m, n, dp);
+    //     int rh = check(i, j-1, m, n, dp);
+    //     return dp[i][j] = lh + rh;
+    // }
 }

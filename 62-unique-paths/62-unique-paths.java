f91c1ddb023796db17c286e0c02e8d16a3ce465a
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2022-08-19 11:41:18 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (60.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -20,16 +20,4 @@ class Solution {
         }
         return dp[m-1][n-1];
     }
-    // public int check(int i, int j, int m, int n, int dp[][])
-    // {
-    //     if(i==0 || j==0)
-    //         return 1;
-    //     else if(i<0 || j<0)
-    //         return 0;
-    //     else if(dp[i][j] != -1)
-    //         return dp[i][j];
-    //     int lh = check(i-1, j, m, n, dp);
-    //     int rh = check(i, j-1, m, n, dp);
-    //     return dp[i][j] = lh + rh;
-    // }
 }

87c6d37e63c5c6fa304e7604b9cdfbfc2b176626
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2022-09-21 22:37:12 +0530

    Time: 1 ms (55.40%), Space: 41.4 MB (17.33%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -3,21 +3,21 @@ class Solution {
         int dp[][] = new int[m][n];
         for(int i=0;i<m; i++)
         {
-            for(int j=0; j<n; j++)
-            {
-                if(i==0 && j==0)
-                    dp[i][j] = 1;
-                else
-                {
-                    int down = 0, right = 0;
-                    if(i > 0)
-                        down = dp[i - 1][j];;
-                    if(j > 0)
-                        right = dp[i][j - 1];
-                    dp[i][j] = down + right;
-                }
-            }
+            for(int j=0;j<n;j++)
+                dp[i][j]=-1;
         }
-        return dp[m-1][n-1];
+        return count(m-1, n-1, dp);
+    }
+    public int count(int i, int j, int dp[][])
+    {
+        if(i==0 && j==0)
+            return 1;
+        if(i<0 || j<0)
+            return 0;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        int left = count(i, j-1, dp);
+        int up = count(i-1, j, dp);
+        return dp[i][j] = (left + up);
     }
 }

fe2da835255222537a10d367c1500e44a388b99c
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2022-09-24 00:02:07 +0530

    Time: 7 ms (15.54%), Space: 45.7 MB (64.46%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -7,32 +7,50 @@ class Solution {
         {
             for(int j=0;j<n;j++)
             {
-                dp[i][j]=-1;
+                dp[i][j]=0;
             }
         }
-        return count(grid, m-1, n-1, dp);
-    }
-    public int count(int[][] grid, int i, int j, int[][] dp)
-    {
-        if(i==0 && j==0)
+        // 
+        for(int i=0;i<m;i++)
         {
-            return grid[i][j];
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0)
+                    dp[i][j] = grid[i][j];
+                else{
+                    int left = grid[i][j];
+                    int up = grid[i][j];
+                    if(j>0) left += dp[i][j-1];
+                    else left = Integer.MAX_VALUE;
+                    if(i>0) up += dp[i-1][j];
+                    else up = Integer.MAX_VALUE;
+                    dp[i][j] = Math.min(left, up);
+                }
+            }
         }
-        if(i<0 || j<0)
-        {
-            return -1;
-        }
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        int left = count(grid, i, j-1, dp);
-        int up = count(grid, i-1, j, dp);
-        if(left!=-1) left += grid[i][j];
-        if(up!=-1) up += grid[i][j];
-        if(left == -1)
-            return dp[i][j] = up;
-        if(up == -1)
-            return dp[i][j] = left;
-        int x = Math.min(up, left);
-        return dp[i][j] = (x);
+        return dp[m-1][n-1];
     }
+    // public int count(int[][] grid, int i, int j, int[][] dp)
+    // {
+    //     if(i==0 && j==0)
+    //     {
+    //         return grid[i][j];
+    //     }
+    //     if(i<0 || j<0)
+    //     {
+    //         return -1;
+    //     }
+    //     if(dp[i][j] != -1)
+    //         return dp[i][j];
+    //     int left = count(grid, i, j-1, dp);
+    //     int up = count(grid, i-1, j, dp);
+    //     if(left!=-1) left += grid[i][j];
+    //     if(up!=-1) up += grid[i][j];
+    //     if(left == -1)
+    //         return dp[i][j] = up;
+    //     if(up == -1)
+    //         return dp[i][j] = left;
+    //     int x = Math.min(up, left);
+    //     return dp[i][j] = (x);
+    // }
 }

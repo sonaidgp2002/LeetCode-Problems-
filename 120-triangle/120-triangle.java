62f3c0ed8ba88b575f7bc141b02654cea47b8763
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2022-09-24 12:36:47 +0530

    Time: 14 ms (13.38%), Space: 43.8 MB (65.22%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -7,21 +7,34 @@ class Solution {
         {
             for(int j=0;j<m;j++)
             {
-                dp[i][j] = -1;
+                dp[i][j] = 0;
             }
         }
-        return count(triangle, n, 0, 0, dp);
-    }
-    public int count(List<List<Integer>> triangle, int n, int i, int j, int[][] dp)
-    {
-        if(i==n-1)
+        for(int j=0;j<m;j++)
         {
-            return triangle.get(i).get(j);
+            dp[n-1][j] = triangle.get(n-1).get(j);
         }
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        int down = triangle.get(i).get(j) + count(triangle, n, i+1, j, dp);
-        int diagonal = triangle.get(i).get(j) + count(triangle, n, i+1, j+1, dp);
-        return dp[i][j] = Math.min(down, diagonal);
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                int down = triangle.get(i).get(j) + dp[i+1][j];
+                int diagonal = triangle.get(i).get(j) + dp[i+1][j+1];
+                dp[i][j] = Math.min(down, diagonal);
+            }
+        }
+        return dp[0][0];
     }
+    // public int count(List<List<Integer>> triangle, int n, int i, int j, int[][] dp)
+    // {
+    //     if(i==n-1)
+    //     {
+    //         return triangle.get(i).get(j);
+    //     }
+    //     if(dp[i][j] != -1)
+    //         return dp[i][j];
+    //     int down = triangle.get(i).get(j) + count(triangle, n, i+1, j, dp);
+    //     int diagonal = triangle.get(i).get(j) + count(triangle, n, i+1, j+1, dp);
+    //     return dp[i][j] = Math.min(down, diagonal);
+    // }
 }

fa2938e683b156347dd1c1bd77f80511d8f3a77e
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2022-09-30 23:17:31 +0530

    Time: 3 ms (5.75%), Space: 41 MB (41.31%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,57 +1,23 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        // int dp[][] = new int[m][n];
-        // for(int i=0;i<m; i++)
-        // {
-        //     for(int j=0;j<n;j++)
-        //         dp[i][j]=0;
-        // }
-        // for(int i=0;i<m;i++)
-        // {
-        //     for(int j=0;j<n;j++)
-        //     {
-        //         if(i==0 && j==0)
-        //             dp[i][j]=1;
-        //         else
-        //         {
-        //             int left =0, up=0;
-        //             if(i>0) up=dp[i-1][j];
-        //             if(j>0) left=dp[i][j-1];
-        //             dp[i][j]=(up+left);
-        //         }
-        //     }
-        // }
-        // // return count(m-1, n-1, dp);
-        // return dp[m-1][n-1];
-        int dp[] = new int[n];
-        for(int i=0;i<n;i++)
-            dp[i]=0;
-        for(int i=0;i<m;i++)
+        int dp[][] = new int[m][n];
+        for(int i=0;i<m; i++)
         {
-            int temp[] = new int[n];
-            for(int j=0;j<n;j++)
+            for(int j=0; j<n; j++)
             {
-                if(j==0)
-                    temp[j]=1;
-                else if(j>0)
+                if(i==0 && j==0)
+                    dp[i][j] = 1;
+                else
                 {
-                    temp[j] = dp[j] + temp[j-1];
+                    int down = 0, right = 0;
+                    if(i > 0)
+                        down = dp[i - 1][j];;
+                    if(j > 0)
+                        right = dp[i][j - 1];
+                    dp[i][j] = down + right;
                 }
             }
-            dp=temp;
         }
-        return dp[n-1];
+        return dp[m-1][n-1];
     }
-    // public int count(int i, int j, int dp[][])
-    // {
-    //     if(i==0 && j==0)
-    //         return 1;
-    //     if(i<0 || j<0)
-    //         return 0;
-    //     if(dp[i][j] != -1)
-    //         return dp[i][j];
-    //     int left = count(i, j-1, dp);
-    //     int up = count(i-1, j, dp);
-    //     return dp[i][j] = (left + up);
-    // }
 }

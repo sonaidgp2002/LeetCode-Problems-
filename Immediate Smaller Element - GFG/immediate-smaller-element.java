74c4ee6a36f6e20d3de5b19a8f6dacbadfffc7f8
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2022-12-16 22:58:35 +0530

    Added solution - LeetHub

--- Immediate Smaller Element - GFG/immediate-smaller-element.java
+++ Immediate Smaller Element - GFG/immediate-smaller-element.java
@@ -38,35 +38,43 @@ public class Main {
 class Solution {
     void immediateSmaller(int arr[], int n) {
         // code 
-        Stack<Integer> stk = new Stack<>();
-        ArrayList<Integer> nums = new ArrayList<>();
-        for(int i=n-1;i>=0;i--)
+        // Stack<Integer> stk = new Stack<>();
+        // ArrayList<Integer> nums = new ArrayList<>();
+        // for(int i=n-1;i>=0;i--)
+        // {
+        //     if(stk.size() == 0)
+        //     nums.add(-1);
+        //     else if(stk.size()>0 && stk.peek()<arr[i])
+        //     {
+        //         if(stk.peek()==arr[i+1] && i<=n-2)
+        //         nums.add(stk.peek());
+        //     }
+        //     else if(stk.size()>0 && stk.peek()>=arr[i])
+        //     {
+        //         while(stk.size()>0 && stk.peek()>=arr[i])
+        //         stk.pop();
+        //         if(stk.size() == 0 )
+        //         nums.add(-1);
+        //         else
+        //         {
+        //             if(stk.peek()==arr[i+1] && i<=n-2)
+        //             nums.add(stk.peek());
+        //             else
+        //             nums.add(-1);
+        //         }
+        //     }
+        //     stk.push(arr[i]);
+        // }
+        // int j = 0;
+        // for(int i=nums.size()-1;i>=0;i--)
+        // arr[j++] = nums.get(i);
+        for(int i=0;i<n-1;i++)
         {
-            if(stk.size() == 0)
-            nums.add(-1);
-            else if(stk.size()>0 && stk.peek()<arr[i])
-            {
-                if(stk.peek()==arr[i+1] && i<=n-2)
-                nums.add(stk.peek());
-            }
-            else if(stk.size()>0 && stk.peek()>=arr[i])
-            {
-                while(stk.size()>0 && stk.peek()>=arr[i])
-                stk.pop();
-                if(stk.size() == 0 )
-                nums.add(-1);
-                else
-                {
-                    if(stk.peek()==arr[i+1] && i<=n-2)
-                    nums.add(stk.peek());
-                    else
-                    nums.add(-1);
-                }
-            }
-            stk.push(arr[i]);
+            if(arr[i]>arr[i+1])
+            arr[i] = arr[i+1];
+            else
+            arr[i] = -1;
         }
-        int j = 0;
-        for(int i=nums.size()-1;i>=0;i--)
-        arr[j++] = nums.get(i);
+        arr[n-1] = -1;
     }
 }

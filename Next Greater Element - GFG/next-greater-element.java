42cc0db2728e46304b2778d66a424a3853c3ee47
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2023-01-10 23:24:27 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.java
+++ Next Greater Element - GFG/next-greater-element.java
@@ -35,35 +35,62 @@ class Solution
     public static long[] nextLargerElement(long[] arr, int n)
     { 
         // Your code here
-        Stack<Long> stk= new Stack<>();  
-        ArrayList<Long> nums = new ArrayList<>();
+        // Stack<Long> stk= new Stack<>();  
+        // ArrayList<Long> nums = new ArrayList<>();
+        // for(int i=n-1;i>=0;i--)
+        // {
+        //     if(stk.size() == 0)
+        //     {
+        //         nums.add( (long)-1);
+        //     }
+        //     else if((stk.size() > 0) && (stk.peek()>arr[i]))
+        //     {
+        //         nums.add(stk.peek());
+        //     }
+        //     else if((stk.size() > 0) && (stk.peek()<=arr[i]))
+        //     {
+        //         while(stk.size()>0 && (stk.peek()<=arr[i]))
+        //         stk.pop();
+        //         if(stk.size() == 0)
+        //         nums.add( (long)-1);
+        //         else
+        //         nums.add(stk.peek());
+        //     }
+        //     stk.push( arr[i] );
+        // }
+        // long ans[] = new long[nums.size()];
+        // int j = 0;
+        // for(int i=nums.size()-1;i>=0;i--)
+        // {
+        //     ans[j++] = nums.get(i);
+        // }
+        // return ans;
+        Stack<Long> stk = new Stack<>();
+        ArrayList<Long> store = new ArrayList<>();
         for(int i=n-1;i>=0;i--)
         {
             if(stk.size() == 0)
             {
-                nums.add( (long)-1);
+                store.add((long)-1);
             }
-            else if((stk.size() > 0) && (stk.peek()>arr[i]))
+            else if(stk.size()>0 && stk.peek()>arr[i])
             {
-                nums.add(stk.peek());
+                store.add(stk.peek());
             }
-            else if((stk.size() > 0) && (stk.peek()<=arr[i]))
+            else if(stk.size()>0 && stk.peek()<=arr[i])
             {
-                while(stk.size()>0 && (stk.peek()<=arr[i]))
+                while(stk.size()>0 && stk.peek()<=arr[i])
                 stk.pop();
                 if(stk.size() == 0)
-                nums.add( (long)-1);
+                store.add((long)-1);
                 else
-                nums.add(stk.peek());
+                store.add(stk.peek());
             }
-            stk.push( arr[i] );
+            stk.push(arr[i]);
         }
-        long ans[] = new long[nums.size()];
         int j = 0;
-        for(int i=nums.size()-1;i>=0;i--)
-        {
-            ans[j++] = nums.get(i);
-        }
-        return ans;
+        for(int i=store.size()-1;i>=0;i--)
+        arr[j++] = store.get(i);
+        return arr;
     } 
 }

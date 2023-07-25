629f5aa7aba59f318f8a4d5fe2dee495369b825b
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2023-07-25 22:53:32 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.java
+++ Subset Sum Problem - GFG/subset-sum-problem.java
@@ -36,34 +36,22 @@ class GFG
 class Solution{
 
 
-    static Boolean isSubsetSum(int N, int arr[], int sum){
-        Boolean dp[][] = new Boolean[N+1][sum+1];
-        for(int i=0;i<N+1;i++)
-        {
-            for(int j=0; j<sum+1; j++)
-            {
-                if(i==0 && j==0)
-                dp[i][j]  = true;
-                else if(j==0)
-                dp[i][j] = true;
-                else if(i==0)
-                dp[i][j] = false;
+    static Boolean isSubsetSum(int n, int arr[], int k){
+        // code here
+        boolean[][] dp = new boolean[n][k+1];
+        for(int i=0;i<n;i++)
+            dp[i][0] = true;
+        if(arr[0] <= k)
+            dp[0][arr[0]] = true;
+        for(int i=1;i<n;i++){
+            for(int j = 0;j<=k;j++){
+                boolean take = false;
+                if(arr[i] <= j)
+                    take = dp[i-1][j - arr[i]];
+                boolean nottake = dp[i-1][j];
+                dp[i][j] = take | nottake;
             }
         }
-        for(int i=1;i<N+1;i++)
-        {
-            for(int j=1; j<sum+1; j++)
-            {
-                if(arr[i-1]<=j)
-                {
-                    dp[i][j] = (dp[i-1][j-arr[i-1]] || dp[i-1][j]);
-                }
-                else
-                {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        return dp[N][sum];
+        return(dp[n-1][k]);
     }
 }

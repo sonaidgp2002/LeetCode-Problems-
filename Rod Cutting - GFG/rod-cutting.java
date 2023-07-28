923f488b75e911d05c55434a7fd6946f6538dfe8
sonaidgp2002 <108181267+sonaidgp2002@users.noreply.github.com>
2023-07-28 12:13:02 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.java
+++ Rod Cutting - GFG/rod-cutting.java
@@ -4,17 +4,21 @@ import java.util.*;
 
 class RodCutting {
 
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+    public static void main(String args[])throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
         while (t-- > 0) {
-            int n = sc.nextInt();
+            int n = Integer.parseInt(in.readLine().trim());
+            String s[]=in.readLine().trim().split(" ");
             int[] arr = new int[n];
-            for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+            for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(s[i]);
 
             Solution ob = new Solution();
-            System.out.println(ob.cutRod(arr, n));
+            out.println(ob.cutRod(arr, n));
         }
+        out.close();
     }
 }
 
@@ -24,26 +28,19 @@ class RodCutting {
 class Solution{
     public int cutRod(int price[], int n) {
         //code here
-        int[] length = new int[n];
-        for(int i=0;i<n;i++)
-            length[i] = i + 1;
-        int sum = n;
-        int[][] dp = new int[n+1][sum+1];
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<sum+1;j++){
-                if(i == 0 || j == 0)
-                    dp[i][j] = 0;
+        int[][] dp = new int[n][n+1];
+        for(int i=0;i<=n;i++)
+            dp[0][i] = i * price[0];
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=n;j++){
+                int take = Integer.MIN_VALUE;
+                int rodlength = i+1;
+                if(rodlength <= j)
+                    take = price[i] + dp[i][j - rodlength];
+                int nottake = dp[i-1][j];
+                dp[i][j] = Math.max(take, nottake);
             }
         }
-        for(int i=1;i<n+1;i++){
-            for(int j=0;j<sum+1;j++){
-                int take = 0;
-                int not_take = dp[i-1][j];
-                if(length[i-1] <= j)
-                    take = price[i-1] + dp[i][j - length[i-1]];
-                dp[i][j] = Math.max(take, not_take);
-            }
-        }
-        return(dp[n][sum]);
+        return(dp[n-1][n]);
     }
 }
